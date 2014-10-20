559fa0a1198097cb81f3a12e9b332f07114a86ea
a-bertrand <a-bertrand@08b0878b-b1a9-4217-8e3a-1912afd769be>
2014-10-20 08:19:08 +0000

    Implemented web interface for the gateway watchdog

--- gateway/GatewayWatchdog/WatchGateway.cs
+++ gateway/GatewayWatchdog/WatchGateway.cs
@@ -10,14 +10,30 @@ using System.Text;
 using System.Threading;
 using PSI.EpicsClient2;
 using System.Collections.Specialized;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
 
 namespace GatewayWatchdog
 {
+    enum GWStatus
+    {
+        STARTING,
+        ALL_OK,
+        HIGH_CPU,
+        NOT_ANSWERING,
+        RESTARTING
+    }
+
     public partial class WatchGateway : ServiceBase
     {
         Thread checkGateway;
         bool shouldStop = false;
         const int nbCPUAvg = 120;
+        TcpListener tcpListener = null;
+        GWStatus status = GWStatus.STARTING;
+        double currentAVG = 0;
+        List<double> lastCPUVals = new List<double>();
 
         public WatchGateway()
         {
@@ -29,24 +45,106 @@ namespace GatewayWatchdog
         {
             checkGateway = new Thread(CheckGateway);
             checkGateway.Start();
+
+            if (ConfigurationManager.AppSettings["WebInterface"] != null)
+            {
+                string s = ConfigurationManager.AppSettings["WebInterface"];
+                IPEndPoint ipSource = new IPEndPoint(IPAddress.Parse(s.Split(':')[0]), int.Parse(s.Split(':')[1]));
+                if (Environment.UserInteractive)
+                    Console.WriteLine("Start receiving HTTP on " + ipSource);
+                tcpListener = new TcpListener(ipSource);
+                tcpListener.Start(10);
+                tcpListener.BeginAcceptSocket(ReceiveConn, tcpListener);
+            }
+        }
+
+        void ReceiveConn(IAsyncResult result)
+        {
+            TcpListener listener = null;
+            Socket client = null;
+
+            try
+            {
+                listener = (TcpListener)result.AsyncState;
+                client = listener.EndAcceptSocket(result);
+
+                client.SetSocketOption(SocketOptionLevel.Tcp, SocketOptionName.NoDelay, true);
+                //Thread.Sleep(100);
+                using (var reader = new StreamReader(new NetworkStream(client)))
+                {
+                    var cmd = reader.ReadLine();
+                    using (var writer = new StreamWriter(new NetworkStream(client)))
+                    {
+                        Console.WriteLine(cmd);
+
+                        if(cmd.StartsWith("POST /restart"))
+                        {
+                            RestartGW();
+                        }
+
+                        writer.WriteLine("HTTP/1.0 OK");
+                        writer.WriteLine("Content-Type: text/html");
+                        writer.WriteLine("Expires: now");
+                        writer.WriteLine();
+                        writer.WriteLine("<html>");
+                        writer.WriteLine("<head><title>Gateway Watchdog - " + ConfigurationManager.AppSettings["GatewayName"] + "</title></head>");
+                        writer.WriteLine("<body>");
+                        writer.WriteLine("Status: " + status + "<br>");
+                        writer.WriteLine("CPU Average: " + currentAVG.ToString("0.00") + "%<br>");
+                        writer.WriteLine("<form method='post' action='/restart'>");
+                        writer.WriteLine("<input type='submit' value='Restart Gateway' onclick='return confirm(\"Are you sure you want to restart the gateway?\");'>");
+                        writer.WriteLine("</form>");
+                        writer.WriteLine("Updated on " + DateTime.Now + "<br>");
+                        writer.WriteLine("<script>setTimeout(\"document.location='/';\",1000);</script>");
+                        writer.WriteLine("</body>");
+                        writer.WriteLine("</html>");
+                        writer.Close();
+                    }
+
+                    reader.Close();
+                }
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                if (Environment.UserInteractive)
+                    Console.WriteLine(ex);
+            }
+
+
+            try
+            {
+                listener.BeginAcceptSocket(new AsyncCallback(ReceiveConn), listener);
+            }
+            catch
+            {
+            }
         }
 
         protected override void OnStart(string[] args)
         {
-            checkGateway = new Thread(CheckGateway);
+            this.Start();
+            /*checkGateway = new Thread(CheckGateway);
             checkGateway.Start();
+
+            if (ConfigurationManager.AppSettings["WebInterface"] != null)
+            {
+            }*/
         }
 
         protected override void OnStop()
         {
             shouldStop = true;
+
+            if (ConfigurationManager.AppSettings["WebInterface"] != null)
+            {
+            }
         }
 
         void CheckGateway()
         {
             if (!Environment.UserInteractive)
                 Thread.Sleep(40000);
-            List<double> lastCPUVals = new List<double>();
 
             NameValueCollection additionalChannels = (NameValueCollection)ConfigurationManager.GetSection("AdditionalChannels");
 
@@ -71,11 +169,14 @@ namespace GatewayWatchdog
                             while (lastCPUVals.Count > nbCPUAvg)
                                 lastCPUVals.RemoveAt(0);
 
-
-                            if (lastCPUVals.Count < nbCPUAvg * 0.8 || lastCPUVals.Average() < 90.0)
+                            currentAVG = lastCPUVals.Average();
+                            if (lastCPUVals.Count < nbCPUAvg * 0.8 || currentAVG < 90.0)
                             {
                                 isOk = true;
+                                status = GWStatus.ALL_OK;
                             }
+                            else
+                                status = GWStatus.HIGH_CPU;
                         }
                         catch
                         {
@@ -97,10 +198,12 @@ namespace GatewayWatchdog
                                     if (Environment.UserInteractive)
                                         Console.WriteLine("Read " + s);
                                     isOk = true;
+                                    status = GWStatus.ALL_OK;
                                 }
                                 catch
                                 {
                                     isOk = false;
+                                    status = GWStatus.NOT_ANSWERING;
                                 }
                             }
                         }
@@ -116,9 +219,7 @@ namespace GatewayWatchdog
                 {
                     if (Environment.UserInteractive)
                         Console.WriteLine("Not ok!!!");
-                    StopGateway();
-                    lastCPUVals.Clear();
-                    StartGateway();
+                    RestartGW();
                     Thread.Sleep(40000);
                 }
                 else
@@ -128,6 +229,14 @@ namespace GatewayWatchdog
                 }
                 Thread.Sleep(10000);
             }
+        }
+
+        void RestartGW()
+        {
+            status = GWStatus.RESTARTING;
+            StopGateway();
+            lastCPUVals.Clear();
+            StartGateway();
         }
 
         void StopGateway()
