28c2ffbffbb365df5df0a3213b291403e9f6ce8e
djlauk <djlauk@08b0878b-b1a9-4217-8e3a-1912afd769be>
2015-04-01 15:24:14 +0000

    Added old reconnection tests. Streamlined namespaces and added license header.

--- channelaccess/ChannelAccessTests/CATypes.cs
+++ channelaccess/ChannelAccessTests/CATypes.cs
@@ -9,7 +9,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace ChannelAccessTests
+namespace EpicsSharp.ChannelAccess.Tests
 {
     [TestClass]
     public class CATypesTest
